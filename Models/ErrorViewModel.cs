40c71a3e3477dc69fa7d499b5876975042c5ab55
sadiamazhar-hp <80788093+sadiamazhar-hp@users.noreply.github.com>
2023-02-15 12:14:04 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace V._3._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
